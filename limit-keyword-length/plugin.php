804d5b3ae401531544ea8e4a047e903f6c970f00
gehwissenlos <manerr@web.de>
2020-12-14 18:06:05 +0100

    Update plugin.php
    Changed to min keyword length

--- limit-keyword-length/plugin.php
+++ limit-keyword-length/plugin.php
@@ -1,26 +1,25 @@
 <?php
 /*
-Plugin Name: Limit keyword length
-Plugin URI: https://github.com/adigitalife/yourls-limit-keyword-length/
-Description: This plugin limits the number of characters for the custom keyword. An error is then returned if the keyword is too long.
+Plugin Name: Limit min keyword length
+Plugin URI: https://github.com/gehwissenlos/yourls-limit-keyword-length/
+Description: This plugin limits the number of minimal characters for the custom keyword. An error is then returned if the keyword is too short.
 Version: 1.0
-Author: Aylwin
-Author URI: http://adigitalife.net/
+Author: gehwissenlos
 */
 
 // Hook our custom function into the 'shunt_add_new_link' filter
-yourls_add_filter( 'shunt_add_new_link', 'limit_keyword_length' );
+yourls_add_filter( 'shunt_add_new_link', 'limit_min_keyword_length' );
 
-// Check the keyword length and return an error if too long
-function limit_keyword_length( $too_long, $url, $keyword ) {
-	$max_keyword_length = 30;
+// Check the keyword length and return an error if too short
+function limit_min_keyword_length( $too_short, $url, $keyword ) {
+	$min_keyword_length = 7;
 	$keyword_length = strlen($keyword);
 
-	if ( $keyword_length > $max_keyword_length ) {
+	if ( $keyword_length < $min_keyword_length ) {
 		$return['status']   = 'fail';
 		$return['code']     = 'error:keyword';
-		$return['message']  = "Sorry, the keyword is too long. It can't be more than " . $max_keyword_length . " characters.";
-		return yourls_apply_filter( 'add_new_link_keyword_too_long', $return );
+		$return['message']  = "Sorry, the keyword is too short. It needs to have at least " . $min_keyword_length . " characters.";
+		return yourls_apply_filter( 'add_new_link_keyword_too_short', $return );
 	}
 
 	return false;
